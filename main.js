5e9b8e70b23e5d8350ed857ae69ecd654607401c
Smari <ellertsmari@gmail.com>
2020-09-17 13:53:02 +0000

    added JavaScript

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("hello world")
+// 3 types of JavaScript
+// Strings - "" +
+// Numbers - 24234 : + - * /
+// Boolean - true/false : && || not
+// JavaScript is Turing complete!!
+"halló heimur" +
+32343 + 
+true && || not
+false
+
+
+4+5
+
+"4" + 5
+"4" + "5"
+
+var name = "Hannes"
